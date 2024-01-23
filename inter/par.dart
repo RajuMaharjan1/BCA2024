ec397bbf966b5fab83c480260e24315c0975e23a
RajuMaharjan1 <rajumaharjan275@gmail.com>
2024-01-23 11:04:18 +0545

    update

---
+++ inter/par.dart
@@ -0,0 +1,26 @@
+void main()//main method. everything runs due to this 
+{
+    //printing hello world and hi in dart pad
+    /*printing hello world 
+    and hi in dart
+    pad*/
+  print("Hello world");
+ print("Raju");
+  //printing name
+  print("rajumaharjan275@gmail.com");
+  //printing email
+  print("19");
+  //printing age
+  
+  String name="RAJU";
+  print("My name is $name");
+  String address="Tokha" ;
+    print("My adderss is $address");
+  int age=19;
+  print("My age is $age");
+  
+  double pi=3.1435;
+  print(pi);
+  
+  
+}
